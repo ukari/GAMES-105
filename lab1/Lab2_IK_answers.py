8e157ba01394a974591c020959c32e060ea57abb
chendianbuji <chendianbuji@gmail.com>
2022-11-20 14:46:34 +0800

    update

--- lab1/Lab2_IK_answers.py
+++ lab1/Lab2_IK_answers.py
@@ -1,5 +1,9 @@
+from __future__ import annotations
 import numpy as np
 from scipy.spatial.transform import Rotation as R
+from typing import Union
+from typing import List
+from typing import Tuple
 
 def part1_inverse_kinematics(meta_data, joint_positions, joint_orientations, target_pose):
     """
@@ -21,8 +25,42 @@ def part1_inverse_kinematics(meta_data, joint_positions, joint_orientations, tar
     joint_initial_position = meta_data.joint_initial_position
     print(joint_initial_position == joint_positions)
     print(joint_positions)
-    
+    print(target_pose)
+    path, path_name, path_end_to_root, path_start_to_root = meta_data.get_path_from_root_to_end()
+    joint_positions, joint_orientations = ccd_once(path_end_to_root, path_start_to_root, joint_positions, joint_orientations, target_pose)
     return joint_positions, joint_orientations
+
+def ccd_once(path_end_to_root: List[int], path_start_to_root: List[int], joint_positions: np.ndarray, joint_orientations: np.ndarray, target_pose: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    positions = joint_positions.copy()
+    orientations = joint_orientations
+    print(path_end_to_root)
+    print(path_start_to_root)
+    joints_num = len(joint_positions)
+    for i in range(len(path_end_to_root) - 1):
+        cur = path_end_to_root[i]
+        prev = path_end_to_root[i + 1]
+        p0 = joint_positions[prev]
+        r0 = joint_positions[cur] - joint_positions[prev]
+        rt = target_pose - joint_positions[prev]
+        cos_theta = r0.dot(rt) / (np.linalg.norm(r0) * np.linalg.norm(rt))
+        theta = -np.rad2deg(np.arccos(cos_theta))
+        print(r0, rt, cos_theta, theta)
+        s = np.sin(theta/2)
+        c = np.cos(theta/2)
+        axis = r0 * rt
+        quat = np.zeros([4])
+        axis_unit = axis / np.linalg.norm(axis)
+        quat[0:3] = axis_unit * s
+        quat[3] = c
+        rotation = R.from_quat(quat)
+        print('deg1', rotation.as_euler('XYZ', degrees=True))
+        r1 = rotation.apply(r0)
+        p1 = r1 + p0
+        print('r0', r0, 'r1', r1,'rt', rt)
+  #      print()
+        positions[cur] = p1
+        orientations[cur] = (R.from_quat(orientations[cur]) * rotation).as_quat()
+    return positions, orientations
 
 def part2_inverse_kinematics(meta_data, joint_positions, joint_orientations, relative_x, relative_z, target_height):
     """
--- lab1/task1_forward_kinematics.py
+++ lab1/task1_forward_kinematics.py
@@ -68,8 +68,10 @@ def part3_retarget(viewer, T_pose_bvh_path, A_pose_bvh_path):
     viewer.run()
 
 def part3_one_pose_debug(viewer, T_pose_bvh_path, A_pose_bvh_path):
-    joint_name, joint_parent, joint_offset = part1_calculate_T_pose(T_pose_bvh_path)
-    retarget_motion_data = part3_retarget_func(T_pose_bvh_path, A_pose_bvh_path)
+    #joint_name, joint_parent, joint_offset = part1_calculate_T_pose(T_pose_bvh_path)
+    #retarget_motion_data = part3_retarget_func(T_pose_bvh_path, A_pose_bvh_path)
+    joint_name, joint_parent, joint_offset = part1_calculate_T_pose(A_pose_bvh_path)
+    retarget_motion_data = load_motion_data(A_pose_bvh_path)
     joint_positions, joint_orientations = part2_forward_kinematics(joint_name, joint_parent, joint_offset, retarget_motion_data, 0)
     viewer.show_pose(joint_name, joint_positions, joint_orientations)
     viewer.run()
@@ -88,8 +90,8 @@ def main():
     # part2_animation(viewer, bvh_file_path)
 
     # part3
-    part3_one_pose_debug(viewer, "data/walk60.bvh", "data/A_pose_run.bvh")
-    #part3_retarget(viewer, "data/walk60.bvh", "data/A_pose_run.bvh")
+    # part3_one_pose_debug(viewer, "data/walk60.bvh", "data/A_pose_run.bvh")
+    part3_retarget(viewer, "data/walk60.bvh", "data/A_pose_run.bvh")
 
 
 if __name__ == "__main__":
